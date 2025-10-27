21709a39e768600df5d73dcd096216d030bce91d
rumbo <rumbo.9370@gmail.com>
2025-10-27 12:15:10 +0100

    models works now, with snake_case

--- Backend/Models/refresh_token.go
+++ Backend/Models/refresh_token.go
@@ -4,13 +4,13 @@ import "time"
 
 type RefreshToken struct {
 	Token     string     `json:"token" gorm:"primaryKey;type:varchar(500)"`
-	UserID    int64      `json:"userID" gorm:"not null;column:userID;"`
-	ExpiresAt time.Time  `json:"expiresAt" gorm:"not null;index:idx_expires_at;column:expiresAt;"`
-	RevokedAt *time.Time `json:"revokedAt" gorm:"index:idx_revoked_at;column:revokedAt;"`
-	Revoked   bool       `json:"revoked" gorm:"not null;default:false;index:idx_revoked;column:revoked;"`
+	UserID    int64      `json:"userID"`
+	ExpiresAt time.Time  `json:"expiresAt" gorm:"index:idx_expires_at"`
+	RevokedAt *time.Time `json:"revokedAt" gorm:"index:idx_revoked_at"`
+	Revoked   bool       `json:"revoked" gorm:"default:false;index:idx_revoked;column:revoked;"`
 	User      User       `json:"user"`
 }
 
-func (RefreshToken) TableName() string {
+/* func (RefreshToken) TableName() string {
 	return "refreshTokens"
-}
+} */
--- Backend/Models/user.go
+++ Backend/Models/user.go
@@ -1,11 +1,13 @@
 package models
 
 type User struct {
-	UserID        int64          `gorm:"primaryKey;type:bigint;autoIncrement" json:"userId"`
-	Name          string         `json:"name"`
-	Age           int            `json:"age"`
-	Email         string         `gorm:"unique" json:"email"`
-	Password      string         `json:"password"`
-	Channels      *Channel       `json:"channels"`
-	RefreshTokens []RefreshToken `json:"refreshTokens"`
+	UserID        int64           `gorm:"primaryKey;type:bigint;autoIncrement" json:"userId"`
+	Name          string          `json:"name"`
+	Age           int             `json:"age"`
+	Email         string          `gorm:"unique" json:"email"`
+	Password      string          `json:"password"`
+	Channels      *Channel        `json:"channels"`
+	RefreshTokens []*RefreshToken `json:"refreshTokens"`
+	VideoHistory  []*Video        `gorm:"many2many:user_video_history;" json:"videoHistory"`
+	WatchLater    []*Video        `gorm:"many2many:user_watch_later;" json:"watchLater"`
 }
