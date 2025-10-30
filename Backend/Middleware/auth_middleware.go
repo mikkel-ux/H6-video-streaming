42359a2edc7d418ea86adf3398003802bc585500
rumbo <rumbo.9370@gmail.com>
2025-10-30 14:58:19 +0100

    auth middleware

--- Backend/Middleware/auth_middleware.go
+++ Backend/Middleware/auth_middleware.go
@@ -1,9 +1,13 @@
 package middleware
 
 import (
+	config "VideoStreamingBackend/Config"
+	models "VideoStreamingBackend/Models"
+	utils "VideoStreamingBackend/Utils"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -12,35 +16,60 @@ import (
 var jwtKey = []byte(os.Getenv("JWT_SECRET"))
 
 type Claims struct {
-	Username string `json:"username"`
+	UserID int64 `json:"user_id"`
 	jwt.RegisteredClaims
+}
+
+func RefreshAccessToken(refreshTokenString string) (string, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(refreshTokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return "", err
+	}
+
+	result := config.DB.Model(&models.RefreshToken{}).
+		Where("token = ? AND revoked = false AND expires_at > ?", refreshTokenString, time.Now()).
+		First(&models.RefreshToken{})
+	if result.Error != nil {
+		return "", result.Error
+	}
+	newAccessToken, err := utils.CreateToken(claims.UserID)
+	if err != nil {
+		return "", err
+	}
+	return newAccessToken, nil
+
 }
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
-			return
-		}
-		println("Token:", authHeader)
+			refreshToken, err := c.Cookie("refresh_token")
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "No authentication provided"})
+				return
+			}
 
-		tokenString := ""
+			newAccessToken, err := RefreshAccessToken(refreshToken)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Failed to refresh access token"})
+				return
+			}
+
+			c.Header("Authorization", "Bearer "+newAccessToken)
+			authHeader = "Bearer " + newAccessToken
+		}
+		var accessToken string
 		if strings.HasPrefix(authHeader, "Bearer ") {
-			tokenString = authHeader[7:]
+			accessToken = authHeader[7:]
 		} else {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
 			return
 		}
-
-		claims := &Claims{}
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			return jwtKey, nil
-		})
-		if err != nil || !token.Valid {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-			return
-		}
+		println(accessToken) //TODO remove debug print
 
 		c.Next()
 	}
