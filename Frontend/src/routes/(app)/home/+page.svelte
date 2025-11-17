ae8b15415267e9289343acd8968ea1e809a51971
rumbo <rumbo.9370@gmail.com>
2025-11-17 23:35:55 +0100

    updated css for home page

--- Frontend/src/routes/(app)/home/+page.svelte
+++ Frontend/src/routes/(app)/home/+page.svelte
@@ -19,17 +19,17 @@
     });
 </script>
 
-<h1 class="text-3xl font-bold underline">
+<h1 class="text-3xl font-bold underline text-center mt-6">
     Welcome to the Home Page!
 </h1>
 
-<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 p-4">
     {#each videos as video}
-        <div class="border rounded-lg overflow-hidden shadow-lg">
-            <img src={`http://localhost:8080/api/images/${video.thumbnail}`} alt={video.title} class="w-full h-48 object-cover" />
-            <div class="p-4">
-                <h2 class="text-xl font-semibold"><a href={`/video/${video.videoId}`}>{video.title}</a></h2>
+        <a href={`/video/${video.videoId}`} class="block group text-center">
+            <div class="w-full h-48 bg-gray-800 rounded-lg overflow-hidden mb-4">
+                <img src={`http://localhost:8080/api/images/${video.thumbnail}`} alt={video.title} class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300" />
             </div>
-        </div>
+            <h2 class="text-lg font-semibold text-white group-hover:text-yellow-300 transition-colors duration-300">{video.title}</h2>
+        </a>
     {/each}
 </div>
